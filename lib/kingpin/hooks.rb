c7e7f136ffe0cc2c85c47aa690f9b4308d23b6df
Johannes Gorset <jgorset@gmail.com>
2012-02-11 21:22:22 +0100

    Refactor hooks

--- lib/kingpin/hooks.rb
+++ lib/kingpin/hooks.rb
@@ -1,8 +1,26 @@
 module Kingpin
 
-  # The Hooks module encapsulates the git hooks that work Kingpin's magic.
+  # The Hooks module encapsulates git hooks.
   module Hooks
-    autoload :PostReceive, "kingpin/hooks/post_receive"
+
+    # The post-receive hook deploys the pushed branch.
+    #
+    # old_revision    - A string describing the commit the repository advanced from.
+    # new_revision    - A string describing the commit the repository advanced to.
+    # reference       - A string describing the reference that was updated.
+    # repository_path - A string describing the path to the repository that was pushed to.
+    def self.post_receive old_revision, new_revision, reference, repository_path
+      destination = "/tmp/checkout"
+      branch      = reference[/refs\/heads\/(.*)/, 1]
+
+      `git clone --branch=#{branch} #{repository_path} #{destination}`
+
+      framework = Frameworks.identify destination
+
+      framework.deploy repository_path, branch
+    ensure
+      `rm -rf #{destination}`
+    end
   end
 
 end
